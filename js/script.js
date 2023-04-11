d4b51744d71564133f9e531dcdfca15596f79781
Leonardo Toccafondi <leonardo.toccafondi1@stud.unifi.it>
2023-04-11 12:40:22 +0200

    Pubblicazione base progetto

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $(".toggle-mnu").on("click", function () {
+        $(this).toggleClass("on");
+        $("#hide").toggleClass("menu-mobile");
+        return false;
+    });
+    $(".cross_news").on("click", function () {
+        $(this).toggleClass("on_m");
+        $("#submenu_m_off_news").toggleClass("submenu_m");
+        return false;
+    });
+    $(".cross_fb").on("click", function () {
+        $(this).toggleClass("on_m");
+        $("#submenu_m_off_fb").toggleClass("submenu_m");
+        return false;
+    });
+    $(".cross_sport").on("click", function () {
+        $(this).toggleClass("on_m");
+        $("#submenu_m_off_sport").toggleClass("submenu_m");
+        return false;
+    });
+    $(".cross_tv").on("click", function () {
+        $(this).toggleClass("on_m");
+        $("#submenu_m_off_tv").toggleClass("submenu_m");
+        return false;
+    });
+});
